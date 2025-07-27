fd7569decc99999f14275c3a618cbf88429fd6fe
arsx9 <sunnysheikh303@gmail.com>
2025-07-28 03:09:20 +0500

    home page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
 import './App.css'
-import Header from './components/Header';
-import HeroSection from './components/HeroSection';
-import Footer from './components/Footer';
-import SearchBar from './components/SearchBar';
-import Listing from './components/Listing';
+import Home from './pages/Home'
+
 function App(){
   return(
-    <div>
-      <Header/>
-      <HeroSection/>
-      <SearchBar/>
-      <Listing/>
-      <Footer/>
-    </div>
+    <Home/>
   );
 }
 export default App;
